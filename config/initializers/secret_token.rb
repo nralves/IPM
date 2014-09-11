78f83191024e61df5646bd258ad39eb5144cecea
Nuno Alves <nr.alves@campus.fct.unl.pt>
2014-09-11 17:43:26 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IPMSite::Application.config.secret_key_base = 'ed0c4ac68319e09defdc6c96ff32c36b08664c6230582d30b45b86f9446f4b006ff3f004c56bb40106abc4d8ee73353271200b61ec39b1450892520679b27b79'
